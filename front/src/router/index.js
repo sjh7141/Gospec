ff6f9d01e6dcfd2ff807f60b9bd503deaf9568aa
tiroring09 <tiroring09@gmail.com>
2020-07-21 10:58:18 +0900

    라우팅테이블 주석 정리
    S03P12A202-7

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,6 +2,27 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+/*
+- 라우팅 테이블 작성시 참고사항 정리 주석
+
+- '@' is an alias to '/src'
+
+- 동적라우트매칭시 변수명에 -대신 _권장
+  ex) path: '/contest/:contest_id'
+  여기서 -쓰면 정규표현식처럼 알아듣는지 의도한대로 동작하지 않음. _로 대체시 정상작동 확인
+
+- 스켈레톤코드에서는 name을 통한 라우팅시에 constant.js를 활용하여 name을 상수관리했다.
+  규모 확장시에 고려해 볼 것.
+
+- route level code-splitting
+  this generates a separate chunk (about.[hash].js) for this route
+  which is lazy-loaded when the route is visited.
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(webpackChunkName: "about" '../views/About.vue')
+  }
+*/
 Vue.use(VueRouter)
 
   const routes = [
@@ -10,14 +31,6 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
   {
     path: '/index',
     component: () => import('../views/Index.vue')
@@ -66,16 +79,6 @@ Vue.use(VueRouter)
       },
     ]
   },
-  /*
-  {
-    path: '/contest/write',
-    component: () => import('../views/contest/ContestWrite.vue')
-  },
-  {
-    path: '/contest/:contest_id', // 아 여기서 -쓰면 정규표현식처럼 알아듣나봄. _로 대체
-    component: () => import('../views/contest/ContestDetail.vue')
-  },
-   */
   {
     path: '/study',
     component: () => import('../views/study/Study.vue'),
