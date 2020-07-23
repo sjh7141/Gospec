2c7a46d12a9f28b69184220df6820ba97ffc54e8
김호연 <hy940313@gmail.com>
2020-07-23 11:13:29 +0900

    공모전 데이터 axios로 가져오기

---
+++ front/src/components/common/ContestData.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+<button type="button" @click="onInputChange">공모전</button>
+<div v-if='view == true'>
+    <p> {{ contests }} </p>
+</div>
+</div>
+  
+</template>
+
+<script>
+import axios from 'axios'
+const API_URL = "http://localhost:8181/api/contest/"
+export default {
+    data() {
+        return {
+        view: false,
+        contests: []
+        }
+    },
+    methods: {
+        onInputChange() {
+      // console.log(inputValue)
+        axios.get(API_URL)
+        .then(response => {
+            this.view = true
+            this.contests = response.data
+            })
+        .catch(error => { console.log(error) })
+        }
+    }
+}
+
+
+</script>
+
+<style>
+
+</style>
--- front/src/views/Schedule.vue
+++ front/src/views/Schedule.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="user" id="login">
         here is schedule<br>
+        <ContestData />
         <router-link to='/contest/1234'>공모전일정 예제 링크(글번호1234)</router-link>
         <CalendarClassify />
 
@@ -10,9 +11,12 @@
 
 <script>
 import CalendarClassify from '../components/common/CalendarClassify.vue'
+import ContestData from '../components/common/ContestData.vue'
+
     export default {
         components: {
             CalendarClassify,
+            ContestData,
         },
         created(){
         },
