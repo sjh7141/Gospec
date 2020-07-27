03ee35b72fdc6b550ccd2f0b6cb7faf81890c3e5
서지훈 <sjh7141@naver.com>
2020-07-27 13:23:20 +0900

    Team Rest Api 추가

--- Back-End/src/main/java/com/gospec/service/TeamService.java
+++ Back-End/src/main/java/com/gospec/service/TeamService.java
@@ -2,9 +2,18 @@ package com.gospec.service;
 
 import java.util.List;
 
+import com.gospec.domain.TeamApprovalDto;
 import com.gospec.domain.TeamDto;
 
 public interface TeamService {
 
 		public List<TeamDto> findAll();
+		
+		public boolean saveTeamApproval(TeamApprovalDto teamApproval);
+		
+		public boolean updateTeamApproval(TeamApprovalDto teamApproval);
+		
+		public boolean deleteTeamApproval(TeamApprovalDto teamApproval);
+		
+		public List<TeamApprovalDto> findTeamById(String teamId);
 }
