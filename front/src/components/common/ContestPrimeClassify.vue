788a601927ad110049feed8d0be92c7972af57ac
tiroring09 <tiroring09@gmail.com>
2020-07-28 12:30:36 +0900

    공모전 게시물 대분류 별로 불러오는 기능 추가
    사용자가 전체/ 기획/ 소프트웨어 등 대분류를 선택함에 따라 불러올 게시물을 바꿔준다
    S03P12A202-62

--- front/src/components/common/ContestPrimeClassify.vue
+++ front/src/components/common/ContestPrimeClassify.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="middle">
         <ul class="horizontal">
-            <li>전체</li>
-            <li>기획/아이디어</li>
-            <li>브랜드/네이밍</li>
-            <li>광고/마케팅</li>
-            <li>사진/영상/UCC</li>
-            <li>디자인</li>
-            <li>예체능</li>
-            <li>문학/수기/시나리오</li>
-            <li>IT/소프트웨어/게임</li>
-            <li>기타</li>
+            <li @click='getContest($event)'>전체</li>
+            <li @click='getContest($event)'>기획/아이디어</li>
+            <li @click='getContest($event)'>광고/마케팅</li>
+            <li @click='getContest($event)'>논문/리포트</li>
+            <li @click='getContest($event)'>영상/UCC/사진</li>
+            <li @click='getContest($event)'>디자인/캐릭터/웹툰</li>
+            <li @click='getContest($event)'>웹/모바일/플래시</li>
+            <li @click='getContest($event)'>게임/소프트웨어</li>
+            <li @click='getContest($event)'>과학/공학</li>
+            <li @click='getContest($event)'>문학/글/시나리오</li>
+            <li @click='getContest($event)'>건축/건설/인테리어</li>
+            <li @click='getContest($event)'>네이밍/슬로건</li>
+            <li @click='getContest($event)'>예체능/미술/음악</li>
+            <li @click='getContest($event)'>대외활동/서포터즈</li>
+            <li @click='getContest($event)'>봉사활동</li>
+            <li @click='getContest($event)'>취업/창업</li>
+            <li @click='getContest($event)'>해외</li>
+            <li @click='getContest($event)'>기타</li>
         </ul>
     </div>
 </template>
 
 <script> 
     export default {
-        name: '',
+        name: 'ContestPrimeClassifyNavBar',
         components: { 
         },
         computed:{
@@ -27,6 +35,14 @@
         created() {
         },
         methods : {
+            getContest(event) {
+                let keyword = this.keywordFilter(event.target.innerHTML);
+                // console.log(keyword);
+                this.$emit('setPrimeFilterKeyword', keyword);
+            },
+            keywordFilter(string) {
+                return string == '전체' ? 'all' : string.replaceAll('/', '-');
+            }
         },
         data: function() {
            return {}
