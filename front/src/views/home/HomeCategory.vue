10b6930722c17a95e14da714381c72a59772a051
김호연 <hy940313@gmail.com>
2020-08-04 16:33:39 +0900

    아이콘 삭제

--- front/src/views/home/HomeCategory.vue
+++ front/src/views/home/HomeCategory.vue
@@ -14,9 +14,6 @@
           @click ="getfield(item.text,item.fieldtype)"
           @change ="getContest"
         >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title v-text="item.fieldtype"></v-list-item-title>
           </v-list-item-content>
@@ -63,99 +60,81 @@ export default {
         item:'',
         field:'all',
         fieldtype: '모든',
-        items: [
+        items:[
         {
-          icon: 'mdi-inbox',
           text: 'all',
           fieldtype: '모든'
         },
         {
-          icon: 'mdi-star',
           text: '기획-아이디어',
           fieldtype: '기획/아이디어'
         },
         {
-          icon: 'mdi-send',
           text: '광고-마케팅',
           fieldtype: '광고/마케팅'
         },
         {
-          icon: 'mdi-email-open',
           text: '논문-리포트',
           fieldtype: '논문/리포트'
         },
         {
-          icon: 'mdi-email-open',
           text: '영상-UCC-사진',
           fieldtype: '영상/UCC/사진'
         },
         {
-          icon: 'mdi-email-open',
           text: '디자인-캐릭터-웹툰',
           fieldtype: '디자인/캐릭터/웹툰'
         },
         {
-          icon: 'mdi-email-open',
           text: '웹-모바일-플래시',
           fieldtype: '웹/모바일/플래시'
         },
         {
-          icon: 'mdi-email-open',
           text: '게임-소프트웨어',
           fieldtype: '게임/소프트웨어'
         },
         {
-          icon: 'mdi-email-open',
           text: '과학-공학',
           fieldtype: '과학/공학'
         },
         {
-          icon: 'mdi-email-open',
           text: '문학-글-시나리오',
           fieldtype: '문학/글/시나리오'
         },
         {
-          icon: 'mdi-email-open',
           text: '건축-건설-인테리어',
           fieldtype: '건축/건설/인테리어'
         },
         {
-          icon: 'mdi-email-open',
           text: '네이밍-슬로건',
           fieldtype: '네이밍/슬로건',
         },
         {
-          icon: 'mdi-email-open',
           text: '예체능-미술-음악',
           fieldtype: '예체능/미술/음악'
         },
         {
-          icon: 'mdi-email-open',
           text: '대외활동-서포터즈',
           fieldtype: '대외활동/서포터즈',
         },
         {
-          icon: 'mdi-email-open',
           text: '봉사활동',
           fieldtype: '봉사활동',
         },
         {
-          icon: 'mdi-email-open',
           text: '취업-창업',
           fieldtype: '취업/창업'
         },
         {
-          icon: 'mdi-email-open',
           text: '해외',
           fieldtype: '해외'
         },
         {
-          icon: 'mdi-email-open',
           text: '기타',
           fieldtype: '기타',
         },
       ],
-      model: 1,
+      model: 0,
     }
 },
 
