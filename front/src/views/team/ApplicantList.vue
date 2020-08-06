906af1e516b1d83f684cb6dde794d571b7ed2204
tiroring09 <tiroring09@gmail.com>
2020-08-06 16:02:33 +0900

    팀원찾기 조회기능 (초안)

--- front/src/views/team/ApplicantList.vue
+++ front/src/views/team/ApplicantList.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
-        This is Contest Applicant List Page.
+        <div v-for="(each, idx) in recomList" :key="idx">
+            {{each.nickname}}/{{each.interestFieldList}}/{{each.username}}으로쪽지기능
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+const URL = 'http://i3a202.p.ssafy.io:8181/api/teams/recommand'
+// const URL = 'http://localhost:8181/api/teams/recommand'
+
 export default {
-    
+    data() {return {
+        recomList : [],
+    }},
+    created() {
+        const config = {
+            headers : {
+                Authorization : this.$cookies.get('auth-token')
+            }
+        };
+
+        axios.get(URL, config)
+            .then(response => {
+                console.dir(response);
+                this.recomList = response.data.user;
+            })
+            .catch(error => console.log(error))
+    },
 }
 </script>
