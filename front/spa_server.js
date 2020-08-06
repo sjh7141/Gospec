249ef290e135388c01bdf41b00dba9d6ffdb0e89
tiroring09 <tiroring09@gmail.com>
2020-08-06 16:19:43 +0900

    배포 서버 관련 주석 보충

--- front/spa_server.js
+++ front/spa_server.js
@@ -4,9 +4,9 @@
     빌드위해서 빠진 패키지 있으면 설치
 2) npm run build
     빌드하여 산출물을 dist디렉토리에 저장
-3-1) node spa_server.js
+3-1) sudo node spa_server.js
     node환경에서 정적파일 제공 목적의 서버를 express사용하여 구현
-3-2) nohup node spa_server.js &
+3-2) nohup sudo node spa_server.js &
     이건 백그라운드에서 실행하도록 하는 명령어
     콘솔을 닫아도 계속 돌아간다
 */
