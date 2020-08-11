5d91e53a519fd75af35ff138b646c473293bf8b6
happy16123 <51432536+happy16123@users.noreply.github.com>
2020-08-11 17:39:44 +0900

    .

--- Back-End/src/main/java/com/gospec/GoSpecApplication.java
+++ Back-End/src/main/java/com/gospec/GoSpecApplication.java
@@ -5,7 +5,6 @@ package com.gospec;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +22,8 @@ import com.gospec.property.FileUploadProperties;
 public class GoSpecApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(GoSpecApplication.class, args);
-		new SpringApplicationBuilder(GoSpecApplication.class).build().run(args);
+		SpringApplication.run(GoSpecApplication.class, args);
+//		new SpringApplicationBuilder(GoSpecApplication.class).build().run(args);
 	}
 
 }
