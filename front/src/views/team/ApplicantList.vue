b20e22ffb0afda2b6c32be80f254030ca10309e6
tiroring-laptop <tiroring09@gmail.com>
2020-08-14 10:01:39 +0900

    팀원추천 페이지 레이아웃, 스타일 개선

--- front/src/views/team/ApplicantList.vue
+++ front/src/views/team/ApplicantList.vue
@@ -1,15 +1,20 @@
 <template>
-<div>
-    <h3>팀원 추천</h3>
-    <div class="loginInfo" v-show="!isLoggedIn">로그인이 필요한 서비스입니다.</div>
-    <div class='mx-auto' style='margin-top: 80px'>
-       <applicant-each v-for="(each, idx) in recomList" :key="idx" :data="each"/>
+<div class="flex_container">
+    <div>
+        <!-- <hr style="margin: 20px;"> -->
+        <h4 class="smallTitle">이 공모전에 관심있는 회원</h4>
+        <div class='mx-auto' style='margin-top: 80px'>
+            <applicant-each v-for="(each, idx) in bookmarkedList.filter( (x, idx) => idx < bookmarkItemCount)" :key="idx" :data="each"/>
+        </div>
+        <button @click="loadBookmarkedPage" v-show="!isLastPage">더보기</button>
     </div>
-    <hr style="margin: 20px;">
-    <h3>이 공모전에 관심있는 회원</h3>
-    <div class="loginInfo" v-show="!isLoggedIn">로그인이 필요한 서비스입니다.</div>
-    <div class='mx-auto' style='margin-top: 80px'>
-    <applicant-each v-for="(each, idx) in bookmarkedList" :key="idx" :data="each"/>
+
+    <div>
+        <h4 class="smallTitle">관심사가 비슷한 회원 추천</h4>
+        <div class="loginInfo" v-show="!isLoggedIn">로그인이 필요한 서비스입니다.</div>
+        <div class='mx-auto' style='margin-top: 80px'>
+            <applicant-each v-for="(each, idx) in recomList" :key="idx" :data="each"/>
+        </div>
     </div>
 </div>
 </template>
@@ -30,9 +35,11 @@ export default {
     data() {return {
         recomList : [],
         bookmarkedList: [],
+        page: 0,
     }},
     created() {
         this.loadList();
+        this.loadBookmarkedPage();
     },
     methods: {
         loadList() {
@@ -45,23 +52,43 @@ export default {
                 })
                 .catch(error => console.log(error))
 
-            axios.get(URL_BOOK + this.$route.params.contest_id, config)
+            axios.get(URL_BOOK + this.$route.params.contest_id)
                 .then(response => {
                     // console.dir(response);
                     this.bookmarkedList = response.data;
                 }).catch(error => console.log(error));
         },
+        loadBookmarkedPage() {
+            this.page++;
+        },
     },
     computed: {
         isLoggedIn() { return this.$cookies.isKey('auth-token'); },
+        isLastPage() { return this.bookmarkItemCount >= this.bookmarkedList.length },
+        bookmarkItemCount() { return this.page * 5 },
     },
 }
 </script>
 
 <style scoped>
+.flex_container {
+    display: flex;
+}
+
+.flex_container > * {
+    flex: 1 1 0;
+}
+
 .loginInfo {
     min-height: 200px;
     line-height: 200px;
     color: gray;
+}
+
+.smallTitle {
+    width: 80%;
+    padding: 15px;
+    margin: 0 auto 30px auto;
+    border-bottom: 1px solid lightgray;
 }
 </style>
